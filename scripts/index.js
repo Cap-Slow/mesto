e945338b3fa316350242701c16c4d8b56b17d570
Dmitrii Liadov <d.liadov@xsolla.com>
2023-01-27 22:04:47 +0400

    add second popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,60 @@
-let container = document.querySelector('.page');
-let editButton = container.querySelector('.profile__edit-button');
-let closeButton = container.querySelector('.popup__close-button');
-let popupForm = container.querySelector('.popup');
-let profileName = container.querySelector('.profile__name');
-let profileJob = container.querySelector('.profile__job');
-let nameInput = popupForm.querySelector('.popup__input_field_name');
-let jobInput = popupForm.querySelector('.popup__input_field_job');
+const container = document.querySelector('.page');
+const editButton = container.querySelector('.profile__edit-button');
+const addButton = container.querySelector('.profile__add-button');
+const popupProfileForm = container.querySelector('.popup-profile');
+const popupPlaceForm = container.querySelector('.popup-place');
+const profileName = container.querySelector('.profile__name');
+const profileJob = container.querySelector('.profile__job');
+const nameInput = popupProfileForm.querySelector('.popup__input_field_name');
+const jobInput = popupProfileForm.querySelector('.popup__input_field_job');
+const profileCloseButton = popupProfileForm.querySelector(
+  '.popup__close-button'
+);
+const placeCloseButton = popupPlaceForm.querySelector(
+  '.popup__close-place-button'
+);
 
-function openPopup() {
-  popupForm.classList.add('popup_opened');
+function openPopup(element) {
+  element.classList.add('popup_opened');
+}
+
+function closePopup(element) {
+  element.classList.remove('popup_opened');
+}
+
+function openProfileModal() {
+  openPopup(popupProfileForm);
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-function closePopup() {
-  popupForm.classList.remove('popup_opened');
+function closeProfileModal() {
+  closePopup(popupProfileForm);
+}
+
+function openPlaceModal() {
+  openPopup(popupPlaceForm);
+  const cardNameInput = popupPlaceForm.querySelector(
+    '.popup__input_field_card-name'
+  );
+  const cardLinkInput = popupPlaceForm.querySelector(
+    '.popup__input_field_card-link'
+  );
+}
+
+function closePlaceModal() {
+  closePopup(popupPlaceForm);
 }
 
 function handleFormSubmit(e) {
   e.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopup();
+  closeProfileModal();
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-popupForm.addEventListener('submit', handleFormSubmit);
+editButton.addEventListener('click', openProfileModal);
+profileCloseButton.addEventListener('click', closeProfileModal);
+placeCloseButton.addEventListener('click', closePlaceModal);
+addButton.addEventListener('click', openPlaceModal);
+popupProfileForm.addEventListener('submit', handleFormSubmit);
