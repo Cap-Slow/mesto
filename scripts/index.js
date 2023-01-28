813d01de38b64593307bd647b4e39254a306e111
Dmitrii Liadov <d.liadov@xsolla.com>
2023-01-28 20:52:09 +0400

    render cards with JS

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,65 @@ const editButton = container.querySelector('.profile__edit-button');
 const addButton = container.querySelector('.profile__add-button');
 const popupProfileForm = container.querySelector('.popup-profile');
 const popupPlaceForm = container.querySelector('.popup-place');
+const cardsContainer = container.querySelector('.elements');
 const profileName = container.querySelector('.profile__name');
 const profileJob = container.querySelector('.profile__job');
-const nameInput = popupProfileForm.querySelector('.popup__input_field_name');
-const jobInput = popupProfileForm.querySelector('.popup__input_field_job');
 const profileCloseButton = popupProfileForm.querySelector(
   '.popup__close-button'
 );
 const placeCloseButton = popupPlaceForm.querySelector(
   '.popup__close-place-button'
 );
+const nameInput = popupProfileForm.querySelector('.popup__input_field_name');
+const jobInput = popupProfileForm.querySelector('.popup__input_field_job');
+const initialCards = [
+  {
+    name: 'Карачаевск',
+    link: '../images/karachaevsk.jpg',
+    alt: 'Тебердинский храм в Карачаево-Черкессии',
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: '../images/elbrus.jpg',
+    alt: 'Гора Эльбрус',
+  },
+  {
+    name: 'Кабардино-Балкария',
+    link: '../images/kabardino-balkaria.jpg',
+    alt: 'Мост через реку в горах Кабардино-Балкарии',
+  },
+  {
+    name: 'Домбай',
+    link: '../images/dombai.png',
+    alt: 'Заснеженная гора на фоне леса',
+  },
+  {
+    name: 'Шаджатмаз',
+    link: '../images/shadzhatmaz.jpg',
+    alt: 'Горное плато с заснеженной вершиной на горизонте',
+  },
+  {
+    name: 'Адыгея',
+    link: '../images/adygea.jpg',
+    alt: 'Маленький деревянный дом на горном склоне',
+  },
+];
+createCards(initialCards);
+
+function createCards(cards) {
+  const cardTemplate = container.querySelector('#card-template').content;
+  cards.forEach((item) => {
+    const cardElement = cardTemplate
+      .querySelector('.elements__item')
+      .cloneNode(true);
+    let cardImage = cardElement.querySelector('.elements__image');
+    let cardTitle = cardElement.querySelector('.elements__title');
+    cardTitle.textContent = item.name;
+    cardImage.src = item.link;
+    cardImage.alt = item.alt;
+    cardsContainer.append(cardElement);
+  });
+}
 
 function openPopup(element) {
   element.classList.add('popup_opened');
@@ -51,6 +100,21 @@ function handleFormSubmit(e) {
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closeProfileModal();
+}
+
+function createCards(cardsArray) {
+  const cardTemplate = container.querySelector('#card-template').content;
+  cardsArray.forEach((item) => {
+    const cardElement = cardTemplate
+      .querySelector('.elements__item')
+      .cloneNode(true);
+    let cardImage = cardElement.querySelector('.elements__image');
+    let cardTitle = cardElement.querySelector('.elements__title');
+    cardTitle.textContent = item.name;
+    cardImage.src = item.link;
+    cardImage.alt = item.alt;
+    cardsContainer.append(cardElement);
+  });
 }
 
 editButton.addEventListener('click', openProfileModal);
