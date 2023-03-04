fa8bbc4a65e2dcc9581e81c4ad940c3cc6102cbb
Dmitrii Liadov <d.liadov@xsolla.com>
2023-03-05 00:08:32 +0400

    feat: add FormValidator class

---
+++ scripts/FormValidator.js
@@ -0,0 +1,77 @@
+export default class FormValidator {
+  constructor(config, formElement) {
+    this._formElement = formElement;
+    this._buttonElement = this._formElement.querySelector(
+      config.submitButtonSelector
+    );
+    this._inactiveButtonClass = config.inactiveButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._inputList = Array.from(
+      this._formElement.querySelectorAll(config.inputSelector)
+    );
+  }
+
+  _showInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(
+      `.${inputElement.name}-error`
+    );
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = inputElement.validationMessage;
+  }
+
+  _hideInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(
+      `.${inputElement.name}-error`
+    );
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.textContent = '';
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _disableSubmitButton() {
+    this._buttonElement.classList.add(this._inactiveButtonClass);
+    this._buttonElement.disabled = true;
+  }
+
+  _enableSubmitButton() {
+    this._buttonElement.classList.remove(this._inactiveButtonClass);
+    this._buttonElement.disabled = false;
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._disableSubmitButton();
+    } else {
+      this._enableSubmitButton();
+    }
+  }
+
+  enableValidation() {
+    this._formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    this._formElement.addEventListener('reset', () => {
+      this._disableSubmitButton();
+    });
+    this._disableSubmitButton();
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+}
