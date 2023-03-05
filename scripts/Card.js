8faf72a9eccf2eda30e949020dff69df7a2f6f2e
Dmitrii Liadov <d.liadov@xsolla.com>
2023-03-05 21:17:21 +0400

    add properties into constructor. Remove method's parameters

--- scripts/Card.js
+++ scripts/Card.js
@@ -2,16 +2,19 @@ export default class Card {
   constructor(name, link, templateElement, handleCardClick) {
     this._name = name;
     this._link = link;
-    this._templateElement = templateElement;
     this._handleCardClick = handleCardClick;
-  }
-
-  _getTemplate() {
-    const clonedCard = document
-      .querySelector(this._templateElement)
+    this._cardElement = document
+      .querySelector(templateElement)
       .content.querySelector('.elements__item')
       .cloneNode(true);
-    return clonedCard;
+    this._imageElement = this._cardElement.querySelector('.elements__image');
+    this._titleElement = this._cardElement.querySelector('.elements__title');
+    this._likeButton = this._cardElement.querySelector(
+      '.elements__like-button'
+    );
+    this._deleteButton = this._cardElement.querySelector(
+      '.elements__delete-button'
+    );
   }
 
   _toggleLike(e) {
@@ -22,24 +25,22 @@ export default class Card {
     e.target.closest('.elements__item').remove();
   }
 
-  _setEventListeners(likeButton, deleteButton, imageElement) {
-    likeButton.addEventListener('click', this._toggleLike);
-    deleteButton.addEventListener('click', this._deleteCard);
-    imageElement.addEventListener('click', () =>
-      this._handleCardClick({ link: this._link, name: this._name })
+  _setEventListeners() {
+    this._likeButton.addEventListener('click', this._toggleLike);
+    this._deleteButton.addEventListener('click', this._deleteCard);
+    this._imageElement.addEventListener('click', () =>
+      this._handleCardClick({
+        link: this._link,
+        name: this._name,
+      })
     );
   }
 
   renderCard() {
-    const cardElement = this._getTemplate();
-    const imageElement = cardElement.querySelector('.elements__image');
-    const titleElement = cardElement.querySelector('.elements__title');
-    const likeButton = cardElement.querySelector('.elements__like-button');
-    const deleteButton = cardElement.querySelector('.elements__delete-button');
-    titleElement.textContent = this._name;
-    imageElement.src = this._link;
-    imageElement.alt = this._name;
-    this._setEventListeners(likeButton, deleteButton, imageElement);
-    return cardElement;
+    this._titleElement.textContent = this._name;
+    this._imageElement.src = this._link;
+    this._imageElement.alt = this._name;
+    this._setEventListeners();
+    return this._cardElement;
   }
 }
