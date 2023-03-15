2ce72a509ff141c13bc885c4c267b8c4cdadcdbf
Dmitrii Liadov <d.liadov@xsolla.com>
2023-03-15 22:57:40 +0400

    refactor: move constants to separate directory

---
+++ utils/constants.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('.page');
+export const editButton = container.querySelector('.profile__edit-button');
+export const addButton = container.querySelector('.profile__add-button');
+const profileForm = document.forms.profile;
+export const cardsSelector = '.elements';
+export const nameInput = profileForm.querySelector('.popup__input_field_name');
+export const jobInput = profileForm.querySelector('.popup__input_field_job');
+export const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+export const config = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_type_disabled',
+  inputErrorClass: 'popup__input_type_error',
+};
