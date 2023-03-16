f932f526d7549ad48762de5d70d8f1385b77c676
Dmitrii Liadov <d.liadov@xsolla.com>
2023-03-16 13:26:56 +0400

    refactor: rename PopupWithImage class file name

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
 import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithImage from '../components/PicturePopup.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
 
