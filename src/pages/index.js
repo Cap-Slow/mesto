ccbf81db5e6867a84137412dc0ed44efc9396304
Dmitrii Liadov <d.liadov@xsolla.com>
2023-03-28 22:20:56 +0400

    feat: add confirmation popup markup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
 import Section from '../components/Section.js';
+import Popup from '../components/Popup.js';
 import PopupWithImage from '../components/PicturePopup.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
@@ -67,6 +68,9 @@ placePopup.setEventListeners();
 
 const imagePopup = new PopupWithImage('.popup-card');
 imagePopup.setEventListeners();
+
+const confirmationPopup = new Popup('.popup__confirmation');
+confirmationPopup.setEventListeners();
 
 const enableValidation = (config) => {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
