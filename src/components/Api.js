c4f57f49d282585480cbbc4da4eac87cf1c8c2d1
Dmitrii Liadov <d.liadov@xsolla.com>
2023-04-02 15:59:04 +0400

    refactor: delete repeating code

--- src/components/Api.js
+++ src/components/Api.js
@@ -4,6 +4,10 @@ export default class Api {
     this._headers = headers;
   }
 
+  _request(url, options) {
+    return fetch(url, options).then(this._handleResponse);
+  }
+
   _handleResponse(res) {
     if (res.ok) {
       return res.json();
@@ -12,60 +16,60 @@ export default class Api {
   }
 
   getInitialCards() {
-    return fetch(`${this._url}cards`, {
+    return this._request(`${this._url}cards`, {
       headers: this._headers,
-    }).then(this._handleResponse);
+    });
   }
 
   getUserInfo() {
-    return fetch(`${this._url}users/me`, {
+    return this._request(`${this._url}users/me`, {
       headers: this._headers,
-    }).then(this._handleResponse);
+    });
   }
 
   setUserInfo(name, about) {
-    return fetch(`${this._url}users/me`, {
+    return this._request(`${this._url}users/me`, {
       method: 'PATCH',
       headers: this._headers,
       body: JSON.stringify({
         name,
         about,
       }),
-    }).then(this._handleResponse);
+    });
   }
 
   setUserAvatar(avatar) {
-    return fetch(`${this._url}users/me/avatar`, {
+    return this._request(`${this._url}users/me/avatar`, {
       method: 'PATCH',
       headers: this._headers,
       body: JSON.stringify({
         avatar,
       }),
-    }).then(this._handleResponse);
+    });
   }
 
   addCard(name, link) {
-    return fetch(`${this._url}cards`, {
+    return this._request(`${this._url}cards`, {
       method: 'POST',
       headers: this._headers,
       body: JSON.stringify({
         name,
         link,
       }),
-    }).then(this._handleResponse);
+    });
   }
 
   deleteCard(cardId) {
-    return fetch(`${this._url}cards/${cardId}`, {
+    return this._request(`${this._url}cards/${cardId}`, {
       method: 'DELETE',
       headers: this._headers,
-    }).then(this._handleResponse);
+    });
   }
 
   toggleLike(cardId, isLiked) {
-    return fetch(`${this._url}cards/likes/${cardId}`, {
+    return this._request(`${this._url}cards/likes/${cardId}`, {
       method: isLiked ? 'DELETE' : 'PUT',
       headers: this._headers,
-    }).then(this._handleResponse);
+    });
   }
 }
